f67121d3db7048d6954a029180c4dbe6950002a2
Michael-Tulmen <michaeltulmen@gmail.com>
2022-01-29 23:01:43 -0600

    added to js file

---
+++ To-Do List /todo.js
@@ -0,0 +1,7 @@
+let addToDo = document.getElementById("addToDo");
+let subtractToDo = document.getElementById("subtractToDo");
+let inputField = document.getElementById('inputField');
+
+addToDo.addEventListener('click', function (){
+    var paragraph=document.createElement('p')
+}
