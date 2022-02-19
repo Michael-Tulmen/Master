bcbbca9b434ba5db68d4550b482e86091c6e7b21
Michael-Tulmen <michaeltulmen@gmail.com>
2022-02-19 12:13:12 -0600

    Virtual Class 1 starting mats

--- To-Do List /todo.js
+++ To-Do List /todo.js
@@ -3,5 +3,12 @@ let subtractToDo = document.getElementById("subtractToDo");
 let inputField = document.getElementById('inputField');
 
 addToDo.addEventListener('click', function (){
-    var paragraph=document.createElement('p')
-}
+    var paragraph=document.createElement('ol')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = inputField.value;
+    todocontainer.appendChild(paragraph);
+    inputField.value = ''
+    paragraph.addEventListener('click', function (){
+     paragraph.style.textDecoration = "line-through";   
+    })
+})
